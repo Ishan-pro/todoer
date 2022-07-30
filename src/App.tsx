95296614fbae19ce0ddf18c8714c6f9f2a50e5bf
Ishan-pro <poet.infamous@gmail.com>
2022-07-30 20:18:21 +0700

    Made the dummy UI with Todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import Navbar from './components/Navbar';
+import TodoForm from './components/TodoForm';
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <Navbar />
+      <TodoForm />
     </>
   );
 }
---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,62 @@
+import Stringifier from 'postcss/lib/stringifier';
+import tw from 'tailwind-styled-components';
+
+interface TodoProps {
+  text: string;
+}
+
+const TodoCom: TodoProps = ({ text }) => {
+  return (
+    <Todo>
+      <span style={{ flex: 1 }}>{text}</span> <Tick type="checkbox" />
+    </Todo>
+  );
+};
+
+const TodoForm = () => {
+  return (
+    <Parent style={{ height: '90vh' }}>
+      <TodoContainer>
+        <TodoCom text={'Hello'} />
+      </TodoContainer>
+      <TodoInput type="text" />
+    </Parent>
+  );
+};
+
+const Parent = tw.div`
+grid
+`;
+
+const Tick = tw.input`
+checked:bg-red-500
+h-30
+w-30
+`;
+
+const Todo = tw.div`
+font-bold
+flex
+flex-row
+h-fit
+text-lg
+`;
+
+const TodoContainer = tw.section`
+h-5/6
+w-5/6
+grid
+place-self-center
+`;
+
+const TodoInput = tw.input`
+bg-black
+text-white
+p-2
+h-fit
+w-5/6
+shadow-lg
+justify-self-center
+`;
+
+export default TodoForm;
