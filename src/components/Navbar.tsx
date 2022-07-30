16655028d49ef0644868335e7c5a2309289be6a0
Ishan-pro <poet.infamous@gmail.com>
2022-07-30 08:17:09 +0700

    Styled up the navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,55 @@
+import tw from 'tailwind-styled-components';
+import { HiMenu } from 'react-icons/hi';
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <Navhero>Todoer</Navhero>
+      <HiMenu size={35} className="lg:hidden" />
+      <Menu>
+        <MenuItems>
+          <L>Dodge</L>
+        </MenuItems>
+        <MenuItems>
+          <L>Dodge</L>
+        </MenuItems>
+        <MenuItems>
+          <L>Dodge</L>
+        </MenuItems>
+        <MenuItems>
+          <L>Dodge</L>
+        </MenuItems>
+      </Menu>
+    </Nav>
+  );
+};
+
+const L = tw.a`
+hover:font-bold
+hover:cursor-pointer
+`;
+
+const MenuItems = tw.li`
+
+`;
+
+const Navhero = tw.div`
+text-xl
+flex-1
+`;
+
+const Nav = tw.nav`
+p-10
+bg-black
+text-white
+flex
+`;
+
+const Menu = tw.ul`
+hidden
+lg:flex
+lg:gap-3
+
+`;
+
+export default Navbar;
